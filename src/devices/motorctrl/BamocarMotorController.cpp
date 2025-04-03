fbe22a99c686a5d272cb0dc4d9aaeb50f8a3c21c
Austin Tsai <aytsai@usc.edu>
2025-04-02 19:53:07 -0700

    fixed motor direction

--- src/devices/motorctrl/BamocarMotorController.cpp
+++ src/devices/motorctrl/BamocarMotorController.cpp
@@ -74,6 +74,7 @@ void BamocarMotorController::setup() {
     enable_sent = false;
     disable_sent = true;
     last_sent_value = 0;
+    mappedMotorTorque = 0;
 }
 
 
@@ -100,7 +101,7 @@ void BamocarMotorController::handleTick() {
     if (throttleRequested > 1400) throttleRequested = 0;
 
     throttleAnalogValue = throttleRequested / 10 * 10; // rounding it to the nearest 10th percent
-    Logger::info("Requested Throttle Percentage: %d", throttleAnalogValue);
+    throttleAnalogValue = 50;
     if (throttleAnalogValue < 50)
     {
         throttleAnalogValue = 0;
@@ -109,23 +110,21 @@ void BamocarMotorController::handleTick() {
             last_sent_value = 0;
             disable_sent = true;
             enable_sent = false;
+            Logger::info("Here1");
+
         }
     }
     else{
-        throttleAnalogValue = throttleAnalogValue/20;
+        // throttleAnalogValue = throttleAnalogValue/20;
         //131071 is 2^17-1 which is in binary is 16 1's since this uses two's complement, this gives you a speed of around -1, as A increases to its max of 100, it will subtract around 2^16-1 from the binary giving you just a leading bit of 1 and a very large negative number as your speed.
-        throttleAnalogValue = 65535 - throttleAnalogValue * 327;
+        mappedMotorTorque = 0;
         // (the following comments disregard the if/else statement)
         // at a = 0 (throttle not pressed), a becomes 2^17 -1 which is 17 1s. When this number is passed through first and second half and through the frame, the 17th bit gets truncated (buf values are 8 bits) --> -1 speed command
         // at a = 1000 (fully pressed), a becomes 65535 which is 16 1s. 
 
-        uint32_t secondhalf = (throttleAnalogValue & 0xFF);
-        uint32_t firsthalf = ((throttleAnalogValue >> 8));
+        uint32_t secondhalf = (mappedMotorTorque & 0xFF);
+        uint32_t firsthalf = ((mappedMotorTorque >> 8));
         
-
-        var.len = 3;
-        var.id = 0x201;
-
         if (!enable_sent){
             var.buf[0] = 0x51;
             var.buf[1] = 0x00;
@@ -133,16 +132,18 @@ void BamocarMotorController::handleTick() {
             attachedCANBus->sendFrame(var);
             disable_sent = false;
             enable_sent = true;
+            Logger::info("Here2");
+
         }
-        if (last_sent_value != throttleAnalogValue){
-            //0x31 for speed, 0x90 for torque
-            var.buf[0] = 0x90;
+        else if (last_sent_value != mappedMotorTorque) //0x31 for speed, 0x90 for torque
+        {
+            Logger::info("Here3");
+            var.buf[0] = 0x31;
             var.buf[1] = secondhalf; //secondhalf
             var.buf[2] = firsthalf; // first half
             attachedCANBus->sendFrame(var);
-            last_sent_value = throttleAnalogValue;
+            last_sent_value = mappedMotorTorque;
         }
-
     }
     
 }
--- src/devices/motorctrl/BamocarMotorController.h
+++ src/devices/motorctrl/BamocarMotorController.h
@@ -88,6 +88,7 @@ private:
     CAN_message_t var; // delete this when handleTick is fully functional
     CAN_message_t freeRolling;
     int throttleAnalogValue;
+    int mappedMotorTorque;
     bool enable_sent;
     bool disable_sent;
     int last_sent_value;
